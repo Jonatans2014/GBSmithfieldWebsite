527dc30c390d4cdd5af6d35e0b518b75544de777
Jonatans2014 <john_Almeida181@outlook.com>
2016-12-02 12:51:10 +0000

    HomePag Added

---
+++ external/js/Carousel.js
@@ -0,0 +1,22 @@
+/**
+ * Created by john on 02/12/2016.
+ */
+$(document).ready(function(){
+    // Activate Carousel
+    $("#myCarousel").carousel();
+
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#myCarousel").carousel("next");
+    });
+});
