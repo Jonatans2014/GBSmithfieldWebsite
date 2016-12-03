8605834eae218a56e4cd4c7225da301dfb4aa268
Jonatans2014 <john_Almeida181@outlook.com>
2016-12-03 15:54:26 +0000

    Login page done

--- external/js/main.js
+++ external/js/main.js
@@ -2,5 +2,7 @@
  * Created by john on 02/12/2016.
  */
 $(document).ready(function(){
-    $('#homepage').fadeIn(2000);
+    $("#myBtn").click(function(){
+        $("#myModal").modal();
+    });
 });
